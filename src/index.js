81fbe7ae81ee44bd8d8bfb4a31ceae082cc484f6
sunflower247 <sunflower247@gmail.com>
2022-08-12 23:21:42 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = str.split('');
+  
+  for (let i=0; i<str.length; i++){
+ 
+    for (let j=0; j<bracketsConfig.length; j++){
+      if (array[i] == bracketsConfig[j][0] && array[i+1] == bracketsConfig[j][1]){
+        array.splice(i,2);
+        if (array.length == 0) return true;
+          else i=-1;
+      }
+    }
+  }
+  
+
+   return false;
 }
